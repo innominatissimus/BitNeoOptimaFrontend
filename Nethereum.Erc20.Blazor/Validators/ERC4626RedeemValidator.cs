28c76459b5f8439070db4897705b3e497686a45b
innominatissimus <innominatissimus@proton.me>
2025-10-24 02:21:56 +0300

    Add full redeem support

---
+++ Nethereum.Erc20.Blazor/Validators/ERC4626RedeemValidator.cs
@@ -0,0 +1,21 @@
+﻿using FluentValidation;
+using Nethereum.UI.Validation;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Nethereum.Erc20.Blazor.Validators
+{
+	public class ERC4626RedeemValidator : AbstractValidator<ERC4626RedeemModel>
+	{
+		public ERC4626RedeemValidator()
+		{
+			RuleFor(t => t.Receiver).IsEthereumAddress();
+			RuleFor(t => t.Shares).GreaterThan(0).WithMessage("Amount must be greater than 0");
+			RuleFor(t => t.ContractModel).SetValidator(new ERC4626ContractValidator());
+			// TODO: Add rule to check that Shares is less than user's balance of vault shares.
+		}
+	}
+}
